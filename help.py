4c83b27c6e441519954103f1a374fcc669d15214
CuttlefishEmpire <24981854+CuttlefishEmpire@users.noreply.github.com>
2020-01-07 16:44:30 -0700

    Initial commit

---
+++ help.py
@@ -0,0 +1,13 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+# ms-python.python added
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), '..\\..\AppData\Local\Temp'))
+	print(os.getcwd())
+except:
+	pass
+# %%
+print("test")
+
